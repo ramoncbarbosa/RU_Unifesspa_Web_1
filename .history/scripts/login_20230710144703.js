393f120f5bbc97ee6d526e91d3c769be9bd871b9
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-07-10 14:58:35 -0300

    fix: login

--- .history/scripts/login_20230710144703.js
+++ .history/scripts/login_20230710144703.js
@@ -8,7 +8,7 @@ loginButton.addEventListener('click', function(){
             let entryUser = document.querySelector('#user').value
             let entryPass = document.querySelector('#password').value
             entryPass = sjcl.codec.hex.fromBits(sjcl.misc.pbkdf2(entryPass, user.salt, 10000, 256));
-            if(entryUser === user.userLogin && entryPass === user.userPassword){
+            if(entryUser == user.userLogin && entryPass == user.userPassword){
                 window.location.href = 'compra-aluno.html';
                 localStorage.setItem('currentUser', keys[i]);
                 return alert('Login efetuado com sucesso.');
