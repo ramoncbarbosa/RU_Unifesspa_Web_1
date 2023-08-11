c5a805ca6dbaa00c4e40e2243d644c4361f91402
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2023-08-11 10:27:18 -0300

    corrigindo bug do carrossel

---
+++ .history/scripts/carrossel_20230811102623.js
@@ -0,0 +1,39 @@
+// const imgs = document.getElementById("img");
+// const img = document.querySelectorAll("#img img");
+
+// let i = 0;
+
+// function carrossel(){
+//     i++;
+
+//     if(i > img.length - 1){
+//         i = 0;
+//     }
+
+//     imgs.style.transform = `translateX(${-i * 40.25}rem)`;
+// }
+
+// setInterval(carrossel, 2400);
+
+
+
+// const carousel = document.querySelector('.carrossel-container');
+// let currentIndex = 0;
+
+// function showImage(index) {
+//     carousel.style.transform = `translateX(-${index * 100}%)`;
+// }
+
+// function nextSlide() {
+//     currentIndex = (currentIndex + 1) % 5;
+//     showImage(currentIndex);
+// }
+
+// function prevSlide() {
+//     currentIndex = (currentIndex - 1 + 5) % 5;
+//     showImage(currentIndex);
+// }
+
+// setInterval(nextSlide, 3000);
+
+// showImage(currentIndex);
